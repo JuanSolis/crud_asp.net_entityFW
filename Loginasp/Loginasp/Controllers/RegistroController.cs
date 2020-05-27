3d06f0ceb3279f1dde95fb1d80f351cc52b6362b
alberto ruano <juansolisgtsk@hotmail.com>
2020-05-26 19:22:08 -0600

    Creacion de base de datos con tabla User y registro de Usuarios

---
+++ Loginasp/Loginasp/Controllers/RegistroController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Loginasp.Models;
+using Loginasp.Util;
+
+namespace Loginasp.Controllers
+{
+    public class RegistroController : Controller
+    {
+
+        public ActionResult Registro()
+        {
+
+            return View("vistaRegistro");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registro(User nuevoUsuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Registro");
+            }
+
+
+            try
+            {
+
+                using (UsuariosDBEntities db = new UsuariosDBEntities())
+                {
+                    db.User.Add(nuevoUsuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+
+                      
+              
+            }
+
+            catch (Exception ex)
+            {
+          
+                ModelState.AddModelError("Error al agregar usuario", ex);
+                ex.Message.ToString();
+                return RedirectToAction("Registro");
+            }
+        }
+            
+    }
+
+}
